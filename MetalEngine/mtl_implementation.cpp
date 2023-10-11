2d30f43aacc06b37b38c5f5ddc48d89ae26940dd
ClémentMNT <clement.montoya@epitech.eu>
2023-10-11 20:29:06 +0200

    Add Metal Lib

---
+++ MetalEngine/mtl_implementation.cpp
@@ -0,0 +1,8 @@
+//
+//  mtl_implementation.cpp
+//  MetalEngine
+//
+//  Created by Clément MONTOYA on 11/10/2023.
+//
+
+#include "mtl_implementation.hpp"
---
+++ MetalEngine/mtl_implementation.hpp
@@ -0,0 +1,13 @@
+//
+//  mtl_implementation.hpp
+//  MetalEngine
+//
+//  Created by Clément MONTOYA on 11/10/2023.
+//
+
+#ifndef mtl_implementation_hpp
+#define mtl_implementation_hpp
+
+#include <stdio.h>
+
+#endif /* mtl_implementation_hpp */
