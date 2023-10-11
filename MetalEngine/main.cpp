50191fecf996cfa6d4cf4a8316be0440fe70f9bc
ClémentMNT <clement.montoya@epitech.eu>
2023-10-11 20:03:20 +0200

    Initial Commit

---
+++ MetalEngine/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  MetalEngine
+//
+//  Created by Clément MONTOYA on 11/10/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
